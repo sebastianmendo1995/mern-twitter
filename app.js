ab5511d83a5496722e87f16047dcd4f3446b1307
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2020-01-20 18:23:21 -0800

    third commit

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true })
